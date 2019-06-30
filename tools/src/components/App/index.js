876cc49d2ebd14641495007cb9989b2203fa2e53
yicj <626659321@qq.com>
2019-06-30 11:35:09 +0800

    s

--- tools/src/components/App/index.js
+++ tools/src/components/App/index.js
@@ -1,15 +1,13 @@
 import React from 'react' ;
-import Button from 'antd/es/button' ;
 import './style.css';
 import GenRequest from '../GenRequest';
-import { Layout, Menu, Breadcrumb } from 'antd';
-const { Header, Content, Footer } = Layout;
 
 class App extends React.Component{
     render(){
         return (
             <div className="App">
                 <GenRequest></GenRequest>
+                这边是执行结果区域
             </div>
         ) ;
     }
