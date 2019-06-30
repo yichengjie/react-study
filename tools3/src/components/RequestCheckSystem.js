7c416bfe26bf195d7edc6f99c3332293bbaf49be
yichengjie <626659321@qq.com>
2019-06-30 17:14:56 +0800

    s

--- tools3/src/components/RequestCheckSystem.js
+++ tools3/src/components/RequestCheckSystem.js
@@ -4,41 +4,41 @@ class RequestCheckSystem extends React.Component{
     constructor(props){
         super(props) ;
         this.state = {
-            sysname:'Dprice'
+            inputType:'search'
         } ;
     }
-    handleChangeSystem(sysname){
+    handleChangeSystem(inputType){
         return (event) => {
-            this.setState({sysname:sysname})
+            this.setState({inputType:inputType})
         }
     }
     render(){
         return (
-            <div>
-                <nav className="navbar navbar-default navbar-static-top">
-                    <p className="navbar-text">系统选择:</p>
-                    <div className="btn-group" role="group" aria-label="Basic example"
-                        style ={{marginTop:"8px"}}>
-                        <button type="button" 
-                                className={this.state.sysname === 'Dprice'? 'btn btn-primary' : 'btn btn-default'} 
-                                onClick={this.handleChangeSystem('Dprice')}>
-                            Dprice
-                        </button>
-                        <button type="button" 
-                                className={this.state.sysname === 'Dreprice'? 'btn btn-primary' : 'btn btn-default'}
-                                onClick={this.handleChangeSystem('Dreprice')}>
-                            Dreprice
-                        </button>
-                        <button type="button" 
-                                className={this.state.sysname === 'Dshop'? 'btn btn-primary' : 'btn btn-default'}
-                                onClick={this.handleChangeSystem('Dshop')}>
-                            Dshop
-                        </button>
-                    </div> 
-                </nav>
-                 
-            </div>
-            
+            <nav className="search-area search-area-defaulta">
+                系统选择:
+                <select>
+                    <option>请选择</option>
+                </select>
+                获取方式:
+                <div className="btn-group" role="group" aria-label="Basic example"
+                    style ={{marginTop:"8px"}}>
+                    <button type="button" 
+                            className={this.state.inputType === 'search'? 'btn btn-primary' : 'btn btn-default'} 
+                            onClick={this.handleChangeSystem('search')}>
+                        生产机搜索
+                    </button>
+                    <button type="button" 
+                            className={this.state.inputType === 'copy'? 'btn btn-primary' : 'btn btn-default'}
+                            onClick={this.handleChangeSystem('copy')}>
+                        复制请求
+                    </button>
+                    <button type="button" 
+                            className={this.state.inputType === 'tam'? 'btn btn-primary' : 'btn btn-default'}
+                            onClick={this.handleChangeSystem('tam')}>
+                        跟踪带
+                    </button>
+                </div> 
+            </nav>
         ) ;
     }
 }
