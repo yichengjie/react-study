2f115e37af7c94cf81ea3db6b9459d42c64a8fd4
yichengjie <626659321@qq.com>
2019-07-02 16:03:35 +0800

    s

---
+++ tools4/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import './App.css';
+import GenRequest from './GenRequest' ;
+
+class App extends React.Component{
+    
+    render(){
+        return (
+            <div className="container">
+                hello world
+            </div>
+        ) ;
+    }
+}
+
+export default App ;
